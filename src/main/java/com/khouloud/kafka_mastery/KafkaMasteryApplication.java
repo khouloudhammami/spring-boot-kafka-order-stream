7031ac33748eb19be2003c65f3777f7e11498702
Khammami-dev <hammamikouloud9@gmail.com>
2026-05-12 12:42:23 +0200

    add config Kafka for spring boot

---
+++ src/main/java/com/khouloud/kafka_mastery/KafkaMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.khouloud.kafka_mastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMasteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khouloud/kafka_mastery/KafkaMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khouloud.kafka_mastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
